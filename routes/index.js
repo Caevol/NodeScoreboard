0a95e2c6c84701fbc4bf66bfece5954c06e577ff
Caevol <logan@loganpedersen.com>
2016-12-06 18:48:28 -0700

    User page functional, connect to game.ejs properly

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
+});
+
+router.post('/game', function(req, res, next){
+  console.log(req.body);
+  res.render('game', {username: req.body.username})
+});
+
+
+router.get('/game', function(req, res, next){
+  res.render('game', {username: "Anonymous" })
 });
 
 module.exports = router;
