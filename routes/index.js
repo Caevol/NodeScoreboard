de065e4bda3d3347bb32468bced83d9978ce5f41
Caevol <logan@loganpedersen.com>
2016-12-06 19:20:40 -0700

    Game interface implemented, high score system implemented.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/scores';
+var db = pgp(connectionString);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +18,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/game', function(req, res, next){
-  console.log(req.body);
   res.render('game', {username: req.body.username})
 });
 
@@ -15,5 +25,31 @@ router.post('/game', function(req, res, next){
 router.get('/game', function(req, res, next){
   res.render('game', {username: "Anonymous" })
 });
+
+router.post('/scores', function(req, res, next){
+  console.log(req.body);
+
+
+
+
+  db.any('SELECT * FROM userscores ORDER BY points LIMIT 5')
+    .then(function(db){
+      return res.render('scores', {db:db});
+    })
+    .catch(function(err){
+      return next(err);
+    })
+});
+
+router.get('/scores', function(req, res, next){
+  db.any('SELECT * FROM userscores ORDER BY points LIMIT 5')
+    .then(function(db){
+      console.log(db);
+      return res.render('scores', {db:db});
+    })
+    .catch(function(err){
+      return next(err);
+    })
+})
 
 module.exports = router;
